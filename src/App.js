1153c79be8ba9f3283e42030a9e521f641a686cb
Yoanna16 <yoannaborisova@gmail.com>
2023-03-16 09:16:08 +0100

    Define Routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Countries from "./components/Countries";
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Country from "./components/Country";
+import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 import Header from "./components/Header";
 import Filter from "./components/Filter";
 
 function App() {
   return (
-    <>
-    <Header />
-    <Filter/>
-    <Countries />
-    </>
+    <Router>
+      <>
+       <Header />
+       <Filter/>
+       <Routes>
+       <Route exact path='/' element={<Countries/>}/>
+       <Route path='/countries/:name' element={<Country />}/>
+     </Routes>
+      </>
+    </Router>
   );
 }
 
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
 const url = "https://restcountries.com/v3.1/all"
 
@@ -16,6 +17,11 @@ const Countries = () => {
         }
         fetchCountryData();
     }, [])
+
+/*     const removeCountry = (ccn3) => {
+      const newCountry = countries.filter((country) => country.ccn3 !== ccn3)
+      setCountries(newCountry);
+    } */
 
 
     return (
@@ -42,6 +48,7 @@ const Countries = () => {
                     <h4>
                       Capital: <span>{capital}</span>
                     </h4>
+                    <Link className='btn' to={`/countries/${name.common}`}> Learn More</Link>
                   </div>
                 </article>
             )
