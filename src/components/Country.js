8a4178e16d6c9ecb7ab9e6de20b862a14dc7cc33
Yoanna16 <yoannaborisova@gmail.com>
2023-03-16 10:23:46 +0100

    Add refinements

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 const Country = () => {
+    const [country, setCoutry] = useState([]);
+
+    const { name } = useParams()
+
+
+    useEffect(() => {
+        const fetchCountyData = async () => {
+            const response = await fetch(
+                `https://restcountries.eu/rest/v2/name/${name}`
+              )
+              const country = await response.json()
+              setCoutry(country)
+        }
+        fetchCountyData()
+        }, [name])
   return (
     <>
+    <Link to='/' className="btn btn-light">
+        <i className='fas fa-arrow-left'>Back Home</i>
+    </Link>
       <h1>Country Data</h1>
     </>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <h1>Where in the world?</h1>
         </div>
         <div>
-            <i className='icon icon-moon'></i>Dark Mode
+            <i className='fas fa-moon'></i>Dark Mode
         </div>
     </header>      
     </>
